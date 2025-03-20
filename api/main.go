129fa114aa941c71fa84a7ee9dea516d3b3914fb
CRaLFa <minami.smd@gmail.com>
2025-03-20 10:01:23 +0900

    create API

---
+++ api/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"cloud.google.com/go/compute/metadata"
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/joho/godotenv"
+)
+
+var jst = time.FixedZone("Asia/Tokyo", 9*60*60)
+
+type tweetData struct {
+	Time      time.Time `json:"time"`
+	Text      string    `json:"text"`
+	Link      string    `json:"link"`
+	CreatedAt string    `json:"createdAt"`
+}
+
+func init() {
+	if !metadata.OnGCE() {
+		if err := godotenv.Load("../.env.yaml"); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello, world!")
+	})
+	mux.HandleFunc("GET /liked-tweets", getLikedTweetsHandler)
+	fmt.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", mux)
+}
+
+func getLikedTweetsHandler(w http.ResponseWriter, r *http.Request) {
+	q := `from(bucket: "twi_fav")
+		|> range(start: 1970-01-01T00:00:00Z)
+		|> filter(fn: (r) => r._measurement == "liked_tweet")
+		|> pivot(rowKey: ["_time"], columnKey: ["_field"], valueColumn: "_value")
+		|> sort(columns: ["_time"], desc: true)
+		|> limit(n: 100)`
+	tweets, err := queryDB(r.Context(), q)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(tweets); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func queryDB(ctx context.Context, query string) ([]tweetData, error) {
+	client := influxdb2.NewClient(os.Getenv("INFLUXDB2_URL"), os.Getenv("INFLUXDB2_TOKEN"))
+	defer client.Close()
+	api := client.QueryAPI(os.Getenv("INFLUXDB2_ORG"))
+	res, err := api.Query(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Close()
+	var tweets []tweetData
+	for res.Next() {
+		rec := res.Record()
+		tweets = append(tweets, tweetData{
+			Time:      rec.Time(),
+			Text:      rec.ValueByKey("text").(string),
+			Link:      rec.ValueByKey("link").(string),
+			CreatedAt: toJST(rec.ValueByKey("createdAt").(string)),
+		})
+	}
+	if res.Err() != nil {
+		return nil, res.Err()
+	}
+	return tweets, nil
+}
+
+func toJST(s string) string {
+	t, err := time.Parse(time.RFC3339, s)
+	if err != nil {
+		panic(err)
+	}
+	return t.In(jst).Format("2006/01/02 15:04")
+}
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eu
+
 deploy_webhook () {
     gcloud --project=sandbox-morita-1-441408 functions deploy twi-fav-webhook \
         --gen2 \
@@ -13,9 +15,26 @@ deploy_webhook () {
         --env-vars-file=./.env.yaml
 }
 
+deploy_api () {
+    gcloud --project=sandbox-morita-1-441408 run deploy twi-fav-api \
+        --region=asia-northeast1 \
+        --source=./api \
+        --allow-unauthenticated \
+        --service-account=cloud-run@sandbox-morita-1-441408.iam.gserviceaccount.com \
+        --env-vars-file=./.env.yaml
+}
+
 main () {
-    cd "$(dirname "$0")" || return
-    deploy_webhook
+    cd "$(dirname "$0")"
+    [[ $# -lt 1 || "$1" = 'webhook' ]] && {
+        echo -e "Deploying webhook...\n"
+        deploy_webhook
+        echo
+    }
+    [[ $# -lt 1 || "$1" = 'api' ]] && {
+        echo -e "Deploying api...\n"
+        deploy_api
+    }
 }
 
 main "$@"
