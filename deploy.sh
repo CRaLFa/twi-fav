092927235bba298c8c6b886a1bc6ec6240821980
CRaLFa <minami.smd@gmail.com>
2025-03-20 20:44:39 +0900

    fix page name

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 set -eu
 
 deploy_webhook () {
-    gcloud --project=sandbox-morita-1-441408 functions deploy twi-fav-webhook \
+    gcloud --project=${1} functions deploy twi-fav-webhook \
         --gen2 \
         --runtime=go123 \
         --region=asia-northeast1 \
@@ -11,29 +11,30 @@ deploy_webhook () {
         --entry-point=SaveLikedTweet \
         --trigger-http \
         --allow-unauthenticated \
-        --service-account=cloud-run@sandbox-morita-1-441408.iam.gserviceaccount.com \
+        --service-account=cloud-run@${1}.iam.gserviceaccount.com \
         --env-vars-file=./.env.yaml
 }
 
 deploy_api () {
-    gcloud --project=sandbox-morita-1-441408 run deploy twi-fav-api \
+    gcloud --project=${1} run deploy twi-fav-api \
         --region=asia-northeast1 \
         --source=./api \
         --allow-unauthenticated \
-        --service-account=cloud-run@sandbox-morita-1-441408.iam.gserviceaccount.com \
+        --service-account=cloud-run@${1}.iam.gserviceaccount.com \
         --env-vars-file=./.env.yaml
 }
 
 main () {
     cd "$(dirname "$0")"
+    local projact_id=$(yq -r '.PROJECT_ID' < .env.yaml)
     [[ $# -lt 1 || "$1" = 'webhook' ]] && {
         echo -e "Deploying webhook...\n"
-        deploy_webhook
+        deploy_webhook "$projact_id"
         echo
     }
     [[ $# -lt 1 || "$1" = 'api' ]] && {
         echo -e "Deploying api...\n"
-        deploy_api
+        deploy_api "$projact_id"
     }
 }
 
