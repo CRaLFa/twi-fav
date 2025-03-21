0cd880a801d61bb24399e913a019aba9e5e11a60
CRaLFa <minami.smd@gmail.com>
2025-03-21 18:10:03 +0900

    update word

--- ui/src/components/NotFound.tsx
+++ ui/src/components/NotFound.tsx
@@ -6,7 +6,7 @@ export const NotFound: TwitterComponents = {
       <div className='react-tweet-theme _root_98iqw_1'>
         <article className='_article_98iqw_21'>
           <div className='_root_16yxa_1'>
-            <h3>センシティブな内容もしくは削除済み</h3>
+            <h3>非公開、センシティブな内容または削除済み</h3>
             <p>
               <a href={`https://x.com/i/status/${error}`} target='_blank'>新しいタブで表示する</a>
             </p>
