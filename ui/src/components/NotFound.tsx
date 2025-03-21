d897a2147dcfb2a993ff720ec38d5f19a26f87fd
CRaLFa <minami.smd@gmail.com>
2025-03-21 21:00:50 +0900

    improve NotFound component

--- ui/src/components/NotFound.tsx
+++ ui/src/components/NotFound.tsx
@@ -6,10 +6,19 @@ export const NotFound: TwitterComponents = {
       <div className='react-tweet-theme _root_98iqw_1'>
         <article className='_article_98iqw_21'>
           <div className='_root_16yxa_1'>
-            <h3>非公開、センシティブな内容または削除済み</h3>
+            <h3>ツイートを表示できません</h3>
+            <p>
+              以下の原因が考えられます
+            </p>
+            <ul>
+              <li>非公開</li>
+              <li>センシティブな内容を含む</li>
+              <li>添付メディアの形式が非対応</li>
+              <li>削除済み</li>
+            </ul>
             <p>
               <a href={`https://x.com/i/status/${error}`} target='_blank' className='_link_1cutb_4'>
-                <span className='_text_1cutb_23'>新しいタブで表示する</span>
+                <span className='_text_1cutb_23'>X で表示する</span>
               </a>
             </p>
           </div>
