050bf947e9f6c85db1d6982e77fd41e00debea2d
CRaLFa <minami.smd@gmail.com>
2025-03-21 18:24:50 +0900

    update style

--- ui/src/components/NotFound.tsx
+++ ui/src/components/NotFound.tsx
@@ -8,7 +8,9 @@ export const NotFound: TwitterComponents = {
           <div className='_root_16yxa_1'>
             <h3>非公開、センシティブな内容または削除済み</h3>
             <p>
-              <a href={`https://x.com/i/status/${error}`} target='_blank'>新しいタブで表示する</a>
+              <a href={`https://x.com/i/status/${error}`} target='_blank' className='_link_1cutb_4'>
+                <span className='_text_1cutb_23'>新しいタブで表示する</span>
+              </a>
             </p>
           </div>
         </article>
